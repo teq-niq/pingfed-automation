66d677e68f8bb6d16aa43a4ba727cf165dba3860
Raghuraman Ramaswamy <raghuraman1@yahoo.com>
2023-03-22 01:31:50 +0530

    save

--- custom-ant-tasks/src/main/java/example/StopNonClusterdPingFed.java
+++ custom-ant-tasks/src/main/java/example/StopNonClusterdPingFed.java
@@ -1,6 +1,9 @@
 package example;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
 import java.util.Optional;
 import java.util.stream.Stream;
 
@@ -20,24 +23,51 @@ public class StopNonClusterdPingFed extends Task {
 		}
 
 	}
-
+	
 	private void inner() throws IOException, InterruptedException {
+		final boolean isOnWin = isOnWin();
+		System.out.println(isOnWin);
 		Stream<ProcessHandle> allProcesses = ProcessHandle.allProcesses();
-		allProcesses.forEach((ProcessHandle h) -> {
-
+		
+		
+		Optional<ProcessHandle> found = allProcesses.parallel().filter((ProcessHandle h) -> {
+			
+			boolean ret=false;
 			long pid = h.pid();
-			Optional<String> commandLine = h.info().commandLine();
+			Optional<String> commandLine = isOnWin?this.getCommandLine(pid):h.info().commandLine();
+			
+
+			
+			
 			if (commandLine.isPresent()) {
 				String cmdLine = commandLine.get();
-				
+				cmdLine=cmdLine.trim();
 				if (cmdLine.endsWith("org.pingidentity.RunPF")) {
-					System.out.println(pid+"\t"+cmdLine.substring(0, 25));
-					kill(pid);
+					
+					ret=true;
 				}
 			}
+			return ret;
 
-		});
+		}).findAny();
+		
+		if(found.isPresent())
+		{
+			kill(found.get().pid());
+		}
+		
+		
 
+	}
+
+	private boolean isOnWin() {
+		String os = System.getProperty("os.name").toLowerCase();
+		boolean isOnWin=false;
+		if(os.contains("win"))
+		{
+			isOnWin=true;
+		}
+		return isOnWin;
 	}
 
 	private void kill(long pid) {
@@ -48,5 +78,40 @@ public class StopNonClusterdPingFed extends Task {
 			System.out.println("called destroy for pid=" + pid);
 		}
 	}
+	
+	public static void main(String[] args) {
+		StopNonClusterdPingFed stopNonClusterdPingFed = new StopNonClusterdPingFed();
+		Optional<String> commandLine = stopNonClusterdPingFed.getCommandLine(26728);
+		if(commandLine.isPresent())
+		{
+			System.out.println(commandLine.get());
+		}
+	}
+	private Optional<String> getCommandLine(long desiredProcessid ) throws UncheckedIOException {
+
+	   
+	    try {
+	      Process process = new ProcessBuilder("wmic", "process", "where", "ProcessID=" + desiredProcessid, "get",
+	        "commandline", "/format:list").
+	        redirectErrorStream(true).
+	        start();
+	      try (InputStreamReader inputStreamReader = new InputStreamReader(process.getInputStream());
+	           BufferedReader reader = new BufferedReader(inputStreamReader)) {
+	        while (true) {
+	          String line = reader.readLine();
+	          
+	          if (line == null) {
+	            return Optional.empty();
+	          }
+	          if (!line.startsWith("CommandLine=")) {
+	            continue;
+	          }
+	          return Optional.of(line.substring("CommandLine=".length()));
+	        }
+	      }
+	    } catch (IOException e) {
+	      throw new UncheckedIOException(e);
+	    }
+	  }
 
 }
